572cc19b7b2f42bd3448321215f3ca61b4f94efc
nuel07 <yehanganep@gmail.com>
2021-09-13 18:08:51 +0000

    task 3 and task 8 corrections

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -6,4 +6,4 @@ def print_reversed_list_integer(my_list=[]):
     if isinstance(my_list, list):
         my_list.reverse()
         for x in my_list():
-            print("{:d}".format[x])
+            print("{:d}".format(x))
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -5,6 +5,6 @@ def multiple_returns(sentence):
     """Return tuple with length of a string and its first character"""
     if sentence = "":
         return (0, None)
-    return(len(sentence), sentence[0])
+    return (len(sentence), sentence[0])
 
         
