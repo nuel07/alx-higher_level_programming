fbfd04e73e9d37527461e8e996b55195e4dfee8f
nuel07 <yehanganep@gmail.com>
2021-09-16 12:20:28 +0000

    all tasks

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def uniq_add(my_list=[]):
+    """adds all unique integers in a list"""
+    result = 0
+    for x in set(my_list):
+        result += x
+    return (result)
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,1 @@
+Dictionaries and sets are the main data structures covered in this directory.
