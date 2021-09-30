e6e1714233bdf72805d51cffef2683d74c3add73
nuel07 <yehanganep@gmail.com>
2021-09-30 01:49:40 -0700

    task 1 pep8 fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,9 +5,8 @@
 class Square:
     """Represents a square"""
     def __init__(self, size):
-        """Initializes square        
-        Args:
+        """Initializes square
+Args:
              size(int): The size of the square
         """
         self.__size = size
-        
