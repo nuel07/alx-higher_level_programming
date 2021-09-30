4477b29333b8a81670a336dbc610e3df0e256e3f
nuel07 <yehanganep@gmail.com>
2021-09-30 04:58:41 -0700

    pep8 fix of task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -14,8 +14,6 @@ Args:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-
-
     def area(self):
         '''Returns current area of square'''
         return (self.__size * self.__size)
