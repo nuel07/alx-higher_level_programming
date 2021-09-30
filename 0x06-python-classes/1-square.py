559e7ca37700e1792b6911033defb89ac30a3d68
nuel07 <yehanganep@gmail.com>
2021-09-30 01:43:08 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """Represents a square"""
+    def __init__(self, size):
+        """Initializes square
+        
+        Args:
+             size(int): The size of the square
+        """
+        self.__size = size
+        
