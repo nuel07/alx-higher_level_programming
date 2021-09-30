f080afdcc74abb55ab17fada9e72c480cc90c4a0
nuel07 <yehanganep@gmail.com>
2021-09-30 04:32:28 -0700

    fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -33,6 +33,7 @@ class Square:
 
     @position.setter
     def position(self, value):
+        """Check eligibility of values"""
         if(not isinstance(value, tuple) or
            len(value) != 2 or
            not all(isinstance(num, int) for num in value) or
