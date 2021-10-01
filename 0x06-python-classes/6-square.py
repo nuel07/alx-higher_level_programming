6d03239a85cd3a457275e1e89ab221663ee492f1
nuel07 <yehanganep@gmail.com>
2021-10-01 00:28:51 -0700

    doc fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,12 +5,12 @@ Define a class square
 
 
 class Square:
-    '''Represents a square'''
+    """Represents a square"""
     def __init__(self, size=0, position=(0,0)):
         """Initialize a new square.
-Args:
-        size (int): The size of the new square.
-        position (int, int): The position of the new square.
+        Args:
+            size (int): The size of the new square.
+            position (int, int): The position of the new square.
         """
         self.size = size
         self.position = position
