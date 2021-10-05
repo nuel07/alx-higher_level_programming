9262bacffae7c598edbe20cb7df63aa9dffd057d
nuel07 <yehanganep@gmail.com>
2021-10-04 23:19:33 -0700

    Initial commits of README, and task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a rectangle"""
+
+
+class Rectangle:
+    """Represents a rectangl"""
+    pass
+r = Rectangle()
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+A deeper dive into classes and objects.
