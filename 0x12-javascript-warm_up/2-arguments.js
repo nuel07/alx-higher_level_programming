dd7da493b65b15927384e8ee7bb79fbab0cc70cb
nuel07 <yehanganep@gmail.com>
2021-12-14 23:33:24 -0800

    task 2 corrected

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const myArgs = process.argv.length;
-if (myArgs > 1){
+const myArgs = process.argv.slice(2).length;
+if (myArgs > 1) {
     console.log('Arguments found');
-} else if (myArgs == 1){
+} else if (myArgs === 1) {
     console.log('Argument found');
 } else {
     console.log('No argument');
