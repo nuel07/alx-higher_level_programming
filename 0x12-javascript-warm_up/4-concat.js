bbd1dc8d89485e15ba9e60d87341e8b15e50d77a
nuel07 <yehanganep@gmail.com>
2021-12-15 00:21:54 -0800

    task 4

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const myConcator = ' is ';
+const myArgs = process.argv.slice(2);
+if (myArgs[0] === undefined && myArgs[1] === undefined){
+    console.log('undefined' + myConcator + 'undefined');
+} else if (myArgs[0] !== undefined && myArgs[1] === undefined){
+    console.log(myArg[0] + myConcator + 'undefined');
+} else if (myArgs[0] !== undefined && myArgs[1] !== undefined){
+    console.log(myArgs[0] + myConcator + myArgs[1]);
+} else if (myArgs[0] === undefined && myArgs[1] !== undefined){
+    console.log('undefined' + myConcator + myArgs[1]);
+}
