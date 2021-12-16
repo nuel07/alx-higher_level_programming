6124d07799e77ca21b4e72a4045a5fe2081aa948
nuel07 <yehanganep@gmail.com>
2021-12-16 03:16:11 -0800

    task fixes

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const myArr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const myArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i<myArr.length; i++) {
     console.log(myArr[i]);
 }
