d614377d6572dee417d14882cda2936739d42284
nuel07 <yehanganep@gmail.com>
2021-12-17 04:23:59 -0800

    task 666666

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+module.exports = class Square extends require ('./5-square.js'){
+    charPrint (c) {
+	if (c === undefined){
+	    this.print();
+	} else {
+	    for (let v = 0; v < this.height; v++){
+		let row = '';
+		for (let r = 0; r < this.width; r++) row += 'X';
+		console.log(row);
+	    }
+	}
+    }
+};
