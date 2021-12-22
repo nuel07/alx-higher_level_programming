9762d0afb3938b05af9eef90cf6104f60f2628b7
nuel07 <yehanganep@gmail.com>
2021-12-21 23:26:50 -0800

    task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  return list.reduceRight(function (arr, p) {
-    return arr.push(p);
-  }, []);
+  const myList = [];
+  while (list.length > 0) {
+    myList.push(list.pop());
+  }
+  return myList;
 };
