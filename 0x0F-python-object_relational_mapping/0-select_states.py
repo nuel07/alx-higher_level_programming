ebe3711514078dcf0f7cc7b2a167627dd8c6fe9b
nuel07 <yehanganep@gmail.com>
2021-12-22 11:32:56 -0800

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,7 +2,7 @@
 
 
 import MySQLdb
-import sys
+from sys import argv
 
 '''
 a script that lists all states
@@ -12,11 +12,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost", port=3306, user=argv[1],
         password=argv[2], database=argv[3])
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    c = cursor.fetchall()
-    for i in c:
-        print(i)
-    cursor.close()
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    for x in c.fetchall():
+        print(x)
     c.close()
+    db.close()
     
