91b5c106377bbd9d746580a20c1141e78bf25c98
nuel07 <yehanganep@gmail.com>
2021-12-22 00:11:53 -0800

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,15 @@
 #!/usr/bin/node
-if (process.argv.length <= 3) {
+const array = process.argv.slice(2);
+let array2 = [];
+function sortNumber (a, b) {
+  return a - b;
+}
+if (array[0] === undefined) {
+  console.log(0);
+} else if (array.length === 1) {
   console.log(0);
 } else {
-  const myArgs = process.argv.map(Number).slice(2, process.argv.length).sort | ((a, b) => a - b);
-  console.log(myArgs[myArgs.length - 2]);
+  array2 = array.sort(sortNumber);
+  array2.pop();
+  console.log(parseInt(array2[array2.length - 1]));
 }
