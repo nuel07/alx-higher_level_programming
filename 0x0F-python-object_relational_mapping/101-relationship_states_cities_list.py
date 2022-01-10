f1ee11c23c90f1c152d72dea7b1196008f162967
nuel07 <yehanganep@gmail.com>
2022-01-10 10:38:13 -0800

    documentation fix

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''
- lists all State objects, and corresponding City objects,
+lists all State objects, and corresponding City objects,
 contained in the database hbtn_0e_101_usa
 '''
 import sys
