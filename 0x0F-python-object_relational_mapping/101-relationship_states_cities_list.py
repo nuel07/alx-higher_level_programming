24c1f2e949ee42b8393e472e43db008b40006844
nuel07 <yehanganep@gmail.com>
2022-01-10 10:46:14 -0800

    code fix

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """
-lists all State objects, and corresponding City objects,
-contained in the database hbtn_0e_101_usa
+All states via SQLAlchemy
 """
-
 import sys
 from relationship_state import Base, State
 from relationship_city import City
@@ -12,7 +10,7 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3],
                                    pool_pre_ping=True))
     Session = sessionmaker(bind=engine)
     session = Session()
