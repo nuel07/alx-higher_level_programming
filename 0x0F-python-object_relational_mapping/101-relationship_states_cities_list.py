af2d1590b22180c1ed8ccd4d25b2937330178c4a
nuel07 <yehanganep@gmail.com>
2022-01-10 10:41:31 -0800

    doc strings fix

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-'''
+"""
 lists all State objects, and corresponding City objects,
 contained in the database hbtn_0e_101_usa
-'''
+"""
+
 import sys
 from relationship_state import Base, State
 from relationship_city import City
