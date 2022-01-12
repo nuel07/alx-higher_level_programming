bd0a68624ca19eb4c078ea9b330f18f4b75a7e69
nuel07 <yehanganep@gmail.com>
2022-01-11 23:11:40 -0800

    doc strings for task 12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''
-changes the name of a State object from the database hbtn_0e_6_usa
+changes the name of a State object 
+from the database hbtn_0e_6_usa
 '''
 
 import sys
@@ -8,7 +9,9 @@ from sqlalchemy import create_engine
 from sqlalachemy.orm import sessionmaker
 from model_state import Base, State
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(sys.argv[1], sys.argv[2], sys.argv[3], pool_pre_ping=True))
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3],
+                                   pool_pre_ping=True))
     Session = sessionmaker(bind=engine)
     session = Session()
     state = session.query(state).filter_by(id=2).first()
