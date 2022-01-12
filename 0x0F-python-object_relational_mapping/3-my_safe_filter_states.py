a34d46762bd672d7061cef4d320c7de0c0598d02
nuel07 <yehanganep@gmail.com>
2022-01-12 01:25:03 -0800

    task 2 and 3 pep fix

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,8 +7,8 @@ But is safe from MySQL injections!
 import sys
 import MySQLdb
 if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM `states`")
     [print(state) for state in cur.fetchall() if state[1] == sys.argv[4]]
-    
