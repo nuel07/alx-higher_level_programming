b0758b7e4edb1ec7565b2c80ae0bf83ceca6a038
nuel07 <yehanganep@gmail.com>
2022-01-12 00:57:07 -0800

    model_state pep fix

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,6 +7,7 @@ from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
+
 class State(Base):
     """
 Represents a state in a MySQL database
