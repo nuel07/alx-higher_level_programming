dda4617e4ca3027c9ff90b6f21dfd7e29743a850
nuel07 <yehanganep@gmail.com>
2022-01-11 23:30:03 -0800

    task 10 fix

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -9,11 +9,12 @@ from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).filter_by(name=argv[4]).first()
+    state = session.query(State).filter_by(name=sys.argv[4]).first()
     if state is not None:
         print("{}".format(state.id))
     else:
