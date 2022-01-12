a8d8eaf6da67d06b0040e5b58a9582daed059160
nuel07 <yehanganep@gmail.com>
2022-01-12 01:12:27 -0800

    task 5 fix

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("""SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN
     states ON cities.state_id = states.id ORDER BY cities.id ASC""")
-    print(", ".join([cty[2] for cty in cur.fetchall() if cty[4] == sys.argv[4]]))
+    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]])
