a95ce01b49570cb3c2c1f9c0436a0a721bdfd16b
nuel07 <yehanganep@gmail.com>
2022-01-12 01:52:28 -0800

    task 9 doc strings fix

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 """lists all State objects that contain the
-
-letter a from the database hbtn_0e_6_usa
-
-"""
+letter a from the database hbtn_0e_6_usa"""
 
 import sys
 from model_state import Base, State
